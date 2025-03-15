927fa44de41897b0ba47eb63d562c7efc19fb78c
Tom Payne <tom@agilepond.com>
2025-03-14 19:03:01 -0700

    Commit - Initial Commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Roboto } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+ 
+export const roboto = Roboto({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
